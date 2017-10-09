6641b1b8fabb7285ebc2e81d7f3f77813efd4846
kassyn <kassyntec@gmail.com>
2017-10-09 10:52:39 -0300

    [Mensagem] Alterado o texto das mensagens de log

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const branch  = process.argv[3];
 const pattern = process.argv[2];
 
 if ( !pattern || !pattern.match(/-p=/) ) {
-	console.log( 'pattern is not defined'.red );
+	console.log( 'Pattern is not defined'.red );
 	process.exit( 1 );
 }
 
 if ( !branch ) {
-	console.log( 'branch is not defined'.red );
+	console.log( 'Branch is not defined'.red );
 	process.exit( 1 );
 }
 
 if ( !verify( branch, pattern.replace( '-p=', '' ) ) ) {
-	console.log( 'branch is not valid user pattern'.red );
+	console.log( 'Your branch is not valid, use the pattern'.red );
 	process.exit( 1 );
 }
 
-console.log( `branch: '${branch}' is valid`.green );
+console.log( `Your branch: '${branch}' is valid`.green );
 
 
